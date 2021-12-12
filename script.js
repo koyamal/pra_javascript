1c32848bd7e981c778150ba0c5fb2fd6441ca2df
koyamal <tanakamaruko1121n@gmail.com>
2021-12-12 14:53:49 +0900

    Create web page using JQuery

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 $(function(){
-  $('#btn1').click(function(){
-    if($('h1').hasClass('show')){
-      $('h1').removeClass('show');
+  $('.method').click(function(){
+    var $method = $(this).find('.hide_content');
+    if($method.hasClass('show')){
+      $method.removeClass('show');
+      $(this).find('span').text('+');
     }else{
-      $('h1').addClass('show');
+      $method.addClass('show');
+      $(this).find('span').text('-');
     }
   });
+
+  $('#trg_fadeout').hover(
+    function(){
+      $('#tgt_fadeout').fadeOut();
+    },
+    function(){
+      $('#tgt_fadeout').fadeIn();
+    }
+  );
+
+  $('#trg_css').hover(
+    function(){
+      $('#tgt_css').css('color', 'red');
+    },
+    function(){
+      $('#tgt_css').css('color', 'black');
+    }
+  );
+
+  $('#trg_text').hover(
+    function(){
+      $('#tgt_text').text('Hello JQuery!');
+    },
+    function(){
+      $('#tgt_text').text('Hello World!');
+    }
+  );
 });
